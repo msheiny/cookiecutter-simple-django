52c24046ca46813df1f519199fbc8700dd28ce78
Michael Sheinberg <m.sheiny@gmail.com>
2013-11-05 20:14:29 -0500

    Adding functional test app scaffolding

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/apps/functional_test/functional_tests.py
@@ -0,0 +1,15 @@
+# Functional tests go here
+
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/testing.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/testing.py
@@ -1,1 +1,3 @@
 from .base import *
+
+INSTALLED_APPS += ('{{cookiecutter.repo_name}}.apps.functional_test',)
